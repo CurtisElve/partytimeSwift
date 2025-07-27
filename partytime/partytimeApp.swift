b0aecefda4d5de18a84a927635060aabfa331083
cuttn <curtis@jelve.com>
2025-07-27 18:40:11 -0400

    Initial Commit

---
+++ partytime/partytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  partytimeApp.swift
+//  partytime
+//
+//  Created by Curtis on 2025-07-27.
+//
+
+import SwiftUI
+
+@main
+struct partytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ partytimeTests/partytimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  partytimeTests.swift
+//  partytimeTests
+//
+//  Created by Curtis on 2025-07-27.
+//
+
+import Testing
+@testable import partytime
+
+struct partytimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
